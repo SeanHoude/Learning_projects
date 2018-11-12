c708f749339464e5f2f391e7fa203f614c8a84a1
Sean Houde <SeanHoude@Gmail.com>
2018-11-12 11:17:34 -0500

    OOP blackjack game now fully functional! Todo - display function

--- blackjack_OOP/blackjack.py
+++ blackjack_OOP/blackjack.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 
 # Generates cards based on the num of decks specified, then shuffles it once. Has a draw card method
@@ -52,7 +53,7 @@ class Hand:
         self.hand = []
 
     def dealer_top_card(self):
-        print(f"The dealers top card is a {self.hand[0]}")
+        print(f"The dealers top card is a {self.hand[1]}.")
 
     # def __Str__(self):
     #     stringy = ", ".join([str(card) for card in self.hand])
@@ -72,38 +73,51 @@ class Game:
         self.chips = 1000
         self.bet = 0
 
+    def clearscreen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def initial_deal(self):
+        self.dealer.hand.clear()
+        self.player.hand.clear()
         for i in range(2):
             self.dealer.hand.append(self.deck.draw_card())
             self.player.hand.append(self.deck.draw_card())
+        self.show_dealer_top_card()
+        self.get_score(self.player)
 
     def hit(self, player):
         player.hand.append(self.deck.draw_card())
-        print(f"a {player.hand[-1]} was drawn")
+        print(f"a {player.hand[-1]} was drawn.")
 
     def display_hand(self):
-        print(f"Your current hand is {self.player.hand}")
+        print(f"Your current hand is {self.player.hand}.")
 
     def hit_or_stay(self):
         choice = None
         while not (choice == 'h' or choice == 's'):
             if choice is not None:
-                print("I didn't understand that, please try again")
+                print("I didn't understand that, please try again.")
             choice = input("Would you like to (h)it or (s)tay?: ").lower()
         return choice == 'h'
 
     def place_bet(self):
-        print(f"You currently have {self.chips} chips")
+        print(f"You currently have {self.chips} chips.")
         self.bet = None
         while not isinstance(self.bet, int):
-            if self.bet is not None:
-                print("I didn't understand that, please try again")
-            bet = input(f"Please place a bet (0 - {self.chips}): ")
+            # if self.bet is not None:
+            #     print("That is an invalid choice. Please try again")
+            self.bet = input(f"Please place a bet (0 - {self.chips}): ")
             try:
-                self.bet = int(bet)
-                print(f"You bet {self.chips} chips on this round.")
-                self.chips -= self.bet
+                self.bet = int(self.bet)
+                if self.bet >= 0 and self.bet <= self.chips:
+                    print(f"You bet {self.bet} chips on this round.")
+                    self.chips -= self.bet
+                else:
+                    self.bet = None
+                    print("You can't bet that number of chips!")
+                    continue
             except:
+                print("That's not a valid answer!")
                 continue
 
     def show_dealer_top_card(self):
@@ -122,20 +136,39 @@ class Game:
             print('You busted!')
         return is_busted
 
+    def dealer_turn(self):
+        while self.get_score(self.dealer) < self.get_score(self.player):
+            self.hit(self.dealer)
+
     def determine_winner(self):
-        print(f"The dealer has {self.get_score(self.dealer)} with a hand of {self.dealer.hand}")
-        if self.get_score(self.player) > self.get_score(self.dealer):
+        print(f"The dealer has {self.get_score(self.dealer)} with a hand of {self.dealer.hand}.")
+        if self.get_score(self.dealer) > 21 or self.get_score(self.player) > self.get_score(self.dealer):
             print('You won the round!!')
             self.chips += (self.bet * 2)
         else:
-            print("The dealer wins this round")
+            print("The dealer won this round.")
+
+    def play_again(self):
+        choice = None
+        while not (choice == 'y' or choice == 'n'):
+            if choice is not None:
+                print("I didn't understand that, please try again.")
+            choice = input("Would you like to play again? (y/n): ").lower()
+        return choice == 'y'
+
+    # def display(self):
+    #     self.clearscreen(self)
+    #     print(f"Current bet: {self.bet}")
+    #     print(f"Chips remaining: {self.chips}")
+    #     print(f"")
+    #     print(f"")
 
     def play_blackjack(self):
         while self.chips > 0:
             self.place_bet()
             self.initial_deal()
-            self.show_dealer_top_card()
             self.display_hand()
+            busted = False
             while self.hit_or_stay():
                 self.hit(self.player)
                 print(f"Your current score is {self.get_score(self.player)}")
@@ -143,10 +176,12 @@ class Game:
                 if busted:
                     break
             if not busted:
+                self.dealer_turn()
                 self.determine_winner()
-        if play_again():
+        print("Looks like you're all out of chips! ")
+        if self.play_again():
             self.chips = 1000
-            play_blackjack(self)
+            self.play_blackjack()
         else:
             print("Thanks for playing!")
 
